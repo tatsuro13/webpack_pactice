969b607df0a689d96ded25fdb470d94b423d5fce
tatsuro13 <oscarwild13@gmail.com>
2020-12-04 17:44:00 +0900

    hashcontentとキャッシュ対策

--- src/js/another.js
+++ src/js/another.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 import velocity from "velocity-animate";
+import { greet } from "./modules/greet";
 
-$("body").css("color", "red");
+$("body").css("color", "red").append(`<p>${greet("Another")}</p>`);
 velocity($("h1"), "fadeIn", {dulation: 2000, loop: true});
 
 // console.log(a);
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import $ from "jquery";
 import velocity from "velocity-animate";
+import { greet } from "./modules/greet";
 import { add } from "./modules/math";
 
 console.log("app");
 
 const result = add(1,2);
 
-$("body").append(result);
+$("body").append(result).append(`<p>${greet("App")}</p>`);
 velocity($("h1"), "fadeIn", {dulation: 2000, loop: true});
 
 // console.log(a);
