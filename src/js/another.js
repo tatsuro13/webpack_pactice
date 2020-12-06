1f181a4af2ed543197630fe6b0145d56551aae41
tatsuro <oscarwild13@gmail.com>
2020-12-06 16:32:58 +0900

    prettier導入

--- src/js/another.js
+++ src/js/another.js
@@ -1,8 +1,10 @@
-import $ from "jquery";
-import velocity from "velocity-animate";
-import { greet } from "./modules/greet";
+import $ from 'jquery';
+import velocity from 'velocity-animate';
+import { greet } from './modules/greet';
 
-$("body").css("color", "red").append(`<p>${greet("Another")}</p>`);
-velocity($("h1"), "fadeIn", {dulation: 2000, loop: true});
+$('body')
+  .css('color', 'red')
+  .append(`<p>${greet('Another')}</p>`);
+velocity($('h1'), 'fadeIn', { dulation: 2000, loop: true });
 
 // console.log(a);
